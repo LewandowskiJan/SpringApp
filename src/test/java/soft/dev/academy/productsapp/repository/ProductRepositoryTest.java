0e3a0e89f31d89d28da3aa89d2c27db646995a59
JanLewandowsi <lewandowski.jan93@gmail.com>
2018-01-20 11:53:58 +0100

    #2 ProductRepository - testing with design pattern -> Assert Object

--- src/test/java/soft/dev/academy/productsapp/repository/ProductRepositoryTest.java
+++ src/test/java/soft/dev/academy/productsapp/repository/ProductRepositoryTest.java
@@ -8,8 +8,10 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import soft.dev.academy.productsapp.asserts.ProductAssert;
 import soft.dev.academy.productsapp.entity.Product;
+import soft.dev.academy.productsapp.entity.ProductType;
 
-import static org.junit.Assert.*;
+import java.util.List;
+
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -19,14 +21,30 @@ public class ProductRepositoryTest {
     private ProductRepository productRepository;
 
     @Test
-    public void shouldFindProductById(){
-       Product product = productRepository.findOne(1);
+    public void shouldFindProductById() {
+        Product product = productRepository.findOne(1);
 
-       new ProductAssert(product).hasName("butter").hasQuantity(5);
+        new ProductAssert(product).hasName("butter").hasQuantity(5);
         /*
         Assert.assertEquals(product.getName(), "butter");
         Assert.assertEquals(product.getQuantity(), new Integer(5));
         */
     }
 
+    @Test
+    public void shouldFindAllProducts() {
+        List<Product> products = (List<Product>) productRepository.findAll();
+        Assert.assertEquals(2, products.size());
+    }
+
+    @Test
+    public void shouldSaveProduct(){
+        //GIVEN
+        Product product = new Product(3, "wine", ProductType.DRINK, 4);
+        //WHEN
+        productRepository.save(product);
+        //THEN
+        Product productFromDatabase = productRepository.findOne(3);
+        new ProductAssert(productFromDatabase).hasName("wine").hasQuantity(4);
+    }
 }
