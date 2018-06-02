bd0043e55b402955fd5e953d6908567c818c1ecb
JanLewandowsi <lewandowski.jan93@gmail.com>
2018-06-02 14:34:52 +0200

    The 8th part of spring-hibernate-tutorial in Udemy. beans scope and lifecycle

--- src/main/java/soft/dev/academy/coachapp/springbeanscopes/BeanScopeDemoApp.java
+++ src/main/java/soft/dev/academy/coachapp/springbeanscopes/BeanScopeDemoApp.java
@@ -1,7 +1,8 @@
 package soft.dev.academy.coachapp.springbeanscopes;
 
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-import soft.dev.academy.coachapp.springdependencyinjectionxml.CricketCoach;
+import soft.dev.academy.coachapp.Coach;
+
 
 public class BeanScopeDemoApp {
 
@@ -12,16 +13,13 @@ public class BeanScopeDemoApp {
                 new ClassPathXmlApplicationContext("beanScope-applicationContext.xml");
 
         // retrieve bean from spring container
-        CricketCoach theCoach = context.getBean("myCricketCoach", CricketCoach.class);
+        Coach theCoach = context.getBean("myCoach", Coach.class);
+
+        Coach alphaCoach = context.getBean("myCoach", Coach.class);
 
         // call methods on the bean
         System.out.println(theCoach.getDailyWorkout());
         System.out.println(theCoach.getDailyFortune());
-
-        //call our new methods to get the literal values
-        System.out.println(theCoach.getEmailAddress());
-
-        System.out.println(theCoach.getTeam());
 
         // close context
         context.close();
