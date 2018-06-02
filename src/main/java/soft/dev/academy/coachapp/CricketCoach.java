7dbdb10c2720acd07d92bd23685633144df95b57
JanLewandowsi <lewandowski.jan93@gmail.com>
2018-06-02 14:02:02 +0200

    The 5th part of spring-hibernate-tutorial in Udemy. This part was about configuration a Spring Beans in contextConfiguration.xml and also about Dependency Injection with injecting LITERAL VALUES.

--- src/main/java/soft/dev/academy/coachapp/CricketCoach.java
+++ src/main/java/soft/dev/academy/coachapp/CricketCoach.java
@@ -4,9 +4,30 @@ public class CricketCoach implements Coach {
 
     private FortuneService fortuneService;
 
+    private String emailAddress;
+    private String team;
+
     // create a no-arg constructor
     public CricketCoach() {
         System.out.println("CricketCoach: inside no-arg constructor");
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public String getTeam() {
+        return team;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        System.out.println("CricketCoach: inside setter method - setEmailAddress");
+        this.emailAddress = emailAddress;
+    }
+
+    public void setTeam(String team) {
+        System.out.println("CricketCoach: inside setter method - setTeam");
+        this.team = team;
     }
 
     // our setter method
--- src/main/java/soft/dev/academy/coachapp/SetterDemoApp.java
+++ src/main/java/soft/dev/academy/coachapp/SetterDemoApp.java
@@ -18,6 +18,11 @@ public class SetterDemoApp {
 
         System.out.println(theCoach.getDailyFortune());
 
+        //call our new methods to get the literal values
+        System.out.println(theCoach.getEmailAddress());
+
+        System.out.println(theCoach.getTeam());
+
         // close context
         context.close();
     }
