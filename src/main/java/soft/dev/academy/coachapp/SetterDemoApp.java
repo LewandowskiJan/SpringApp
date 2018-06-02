440e10dfcd29998b486063e1fd33de90d2091cb1
JanLewandowsi <lewandowski.jan93@gmail.com>
2018-06-02 13:50:57 +0200

    The 4th part of spring-hibernate-tutorial in Udemy. This part was about configuration a Spring Beans in contextConfiguration.xml and also about Dependency Injection using SETTER INJECTION.

---
+++ src/main/java/soft/dev/academy/coachapp/SetterDemoApp.java
@@ -0,0 +1,25 @@
+package soft.dev.academy.coachapp;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class SetterDemoApp {
+
+    public static void main(String[] args) {
+
+        // load the spring configuration file
+        ClassPathXmlApplicationContext context =
+                new ClassPathXmlApplicationContext("applicationContext.xml");
+
+        // retrieve bean from spring container
+        CricketCoach theCoach = context.getBean("myCricketCoach", CricketCoach.class);
+
+        // call methods on the bean
+        System.out.println(theCoach.getDailyWorkout());
+
+        System.out.println(theCoach.getDailyFortune());
+
+        // close context
+        context.close();
+    }
+
+}
