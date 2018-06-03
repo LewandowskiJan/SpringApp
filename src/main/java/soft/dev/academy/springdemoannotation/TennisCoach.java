977fb751730ab6dd14fbe9f9c3adf7cd66c5e599
JanLewandowsi <lewandowski.jan93@gmail.com>
2018-06-03 17:29:29 +0200

    The 13th part of spring-hibernate-tutorial in Udemy. Spring Scope with annotation scope = prototype

--- src/main/java/soft/dev/academy/springdemoannotation/TennisCoach.java
+++ src/main/java/soft/dev/academy/springdemoannotation/TennisCoach.java
@@ -2,10 +2,12 @@ package soft.dev.academy.springdemoannotation;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 
 @Component
+@Scope("prototype")
 public class TennisCoach implements Coach {
 
 
