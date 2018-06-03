2f4879c669fb0c1e9e15e799d4d495851eba0344
JanLewandowsi <lewandowski.jan93@gmail.com>
2018-06-03 14:50:40 +0200

    The 11th part of spring-hibernate-tutorial in Udemy. Spring with @Autowired annotation Setter Injection

--- src/main/java/soft/dev/academy/springdemoannotation/TennisCoach.java
+++ src/main/java/soft/dev/academy/springdemoannotation/TennisCoach.java
@@ -9,10 +9,26 @@ public class TennisCoach implements Coach {
 
     private FortuneServie fortuneServie;
 
+    // define a default constructor
+
+    public TennisCoach() {
+        System.out.println(">> TennisCoach: inside default constructor");
+    }
+
+    // define a setter metod
+    @Autowired
+    public void setFortuneServie(FortuneServie theFortuneServie) {
+        this.fortuneServie = theFortuneServie;
+    }
+
+
+
+    /*
     @Autowired
     public TennisCoach(FortuneServie theFortuneServie) {
         fortuneServie = theFortuneServie;
     }
+    */
 
     @Override
     public String getDailyWorkout() {
