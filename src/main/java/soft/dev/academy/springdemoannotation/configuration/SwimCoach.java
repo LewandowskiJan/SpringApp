97195f7505a2a4b19682fa28568e4711fbb239d8
JanLewandowsi <lewandowski.jan93@gmail.com>
2018-06-03 19:26:01 +0200

    The 16th part of spring-hibernate-tutorial in Udemy. PropertySource annotation -> getting value from .properties file print these values

--- src/main/java/soft/dev/academy/springdemoannotation/configuration/SwimCoach.java
+++ src/main/java/soft/dev/academy/springdemoannotation/configuration/SwimCoach.java
@@ -1,5 +1,6 @@
 package soft.dev.academy.springdemoannotation.configuration;
 
+import org.springframework.beans.factory.annotation.Value;
 import soft.dev.academy.springdemoannotation.Coach;
 import soft.dev.academy.springdemoannotation.FortuneService;
 
@@ -7,6 +8,12 @@ public class SwimCoach implements Coach {
 
 
     private FortuneService fortuneService;
+
+    @Value("${foo.email}")
+    private String email;
+
+    @Value("${foo.team}")
+    private String team;
 
     public SwimCoach(FortuneService theFortuneService) {
         this.fortuneService = theFortuneService;
@@ -20,5 +27,13 @@ public class SwimCoach implements Coach {
     @Override
     public String getDailyFortune() {
         return fortuneService.getFortune();
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getTeam() {
+        return team;
     }
 }
--- src/main/java/soft/dev/academy/springdemoannotation/configuration/SwimJavaConfigDemoApp.java
+++ src/main/java/soft/dev/academy/springdemoannotation/configuration/SwimJavaConfigDemoApp.java
@@ -11,13 +11,17 @@ public class SwimJavaConfigDemoApp {
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(SportConfig.class);
 
         // get the bea from spring container
-        Coach theCoach = context.getBean("swimCoach", Coach.class);
+        SwimCoach theCoach = context.getBean("swimCoach", SwimCoach.class);
 
         // call a method on the bean
         System.out.println(theCoach.getDailyWorkout());
 
         // call method to get the daily fortune
         System.out.println(theCoach.getDailyFortune());
+
+        // call our new swim coach methods ... has the props values injected
+        System.out.println("email: " + theCoach.getEmail());
+        System.out.println("team: " + theCoach.getTeam());
 
         // close the context
         context.close();
