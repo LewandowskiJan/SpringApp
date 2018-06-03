e7dbecc28def773573979088cf95ea748620941a
JanLewandowsi <lewandowski.jan93@gmail.com>
2018-06-03 17:28:11 +0200

    The 13th part of spring-hibernate-tutorial in Udemy. Spring Scope with annotation

--- src/main/java/soft/dev/academy/springdemoannotation/TennisCoach.java
+++ src/main/java/soft/dev/academy/springdemoannotation/TennisCoach.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Component;
 public class TennisCoach implements Coach {
 
 
-    @Qualifier("happyFortuneService")
+    @Qualifier("randomFortuneService")
     @Autowired
     private FortuneService fortuneService;
 
---
+++ src/main/java/soft/dev/academy/springdemoannotation/service/RandomFortuneService.java
@@ -0,0 +1,30 @@
+package soft.dev.academy.springdemoannotation.service;
+
+import org.springframework.stereotype.Component;
+import soft.dev.academy.springdemoannotation.FortuneService;
+
+import java.util.Random;
+
+@Component
+public class RandomFortuneService implements FortuneService {
+
+    // create an array of string
+    private String[] data = {
+            "Beware of the wolf in sheep's clothing",
+            "Diligence is the mother of good luck",
+            "The journey is the reward"
+    };
+
+    // create a random number generator
+    private Random myRandom = new Random();
+
+    @Override
+    public String getFortune() {
+        // pick a random string from the array
+        int index = myRandom.nextInt(data.length);
+
+        String theFortune = data[index];
+
+        return theFortune;
+    }
+}
