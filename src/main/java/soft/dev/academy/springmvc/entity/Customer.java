01967fdb564280da3d8ae5484ccfbbd73b823102
JanLewandowsi <lewandowski.jan93@gmail.com>
2018-06-07 21:09:56 +0200

    #3 Spring-mvc demo app - Spring Form Validation with Hibernate Validator, solve problem with "   ". To solve that issue we used @InitBinder

--- src/main/java/soft/dev/academy/springmvc/entity/Customer.java
+++ src/main/java/soft/dev/academy/springmvc/entity/Customer.java
@@ -7,7 +7,7 @@ public class Customer {
 
     private String firstName;
 
-    @NotNull
+    @NotNull(message = "is required")
     @Size(min=1, message = "is required")
     private String lastName;
 
