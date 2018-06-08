b37ea70ffd2d6350dc710d81f8d86d692ec96afd
JanLewandowsi <lewandowski.jan93@gmail.com>
2018-06-08 20:58:48 +0200

    #5 Spring-mvc demo app - Spring Form Validation with Hibernate Validator, using case: error type with int - must be change to Integer

--- src/main/java/soft/dev/academy/springmvc/entity/Customer.java
+++ src/main/java/soft/dev/academy/springmvc/entity/Customer.java
@@ -1,7 +1,5 @@
 package soft.dev.academy.springmvc.entity;
 
-import org.springframework.web.bind.annotation.InitBinder;
-
 import javax.validation.constraints.*;
 
 public class Customer {
@@ -12,10 +10,10 @@ public class Customer {
     @Size(min = 1, message = "is required")
     private String lastName;
 
-
+    @NotNull(message = "is required")
     @Min(value = 0, message = "must be greater than or equal to zero")
     @Max(value = 10, message = "must be less than or equal to 10")
-    private int freePasses;
+    private Integer freePasses;
 
     @Pattern(regexp = "^[a-zA-Z0-9]{5}", message = "only 5 chars/digits")
     private String postalCode;
@@ -28,11 +26,11 @@ public class Customer {
         this.postalCode = postalCode;
     }
 
-    public int getFreePasses() {
+    public Integer getFreePasses() {
         return freePasses;
     }
 
-    public void setFreePasses(int freePasses) {
+    public void setFreePasses(Integer freePasses) {
         this.freePasses = freePasses;
     }
 
