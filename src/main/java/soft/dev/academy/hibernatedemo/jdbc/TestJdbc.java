f5b23e208a9a73349383a26b4c050dc33805b18c
JanLewandowsi <lewandowski.jan93@gmail.com>
2018-06-09 16:15:10 +0200

    #6 Spring-mvc demo app - MySQL and Hibernate Configuration

---
+++ src/main/java/soft/dev/academy/hibernatedemo/jdbc/TestJdbc.java
@@ -0,0 +1,28 @@
+package soft.dev.academy.hibernatedemo.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class TestJdbc {
+
+    public static void main(String[] args) {
+
+        String jdbcUrl = "jdbc:mysql://localhost:3306/hb_student_tracker?useSSL=false";
+        String user = "hbstudent";
+        String pass = "hbstudent";
+
+        try{
+
+            System.out.println("Connecting to database " + jdbcUrl);
+
+            Connection connection = DriverManager.getConnection(jdbcUrl, user, pass);
+
+            System.out.println("Connecting successful!");
+
+        } catch (Exception exc){
+            exc.printStackTrace();
+        }
+
+    }
+
+}
