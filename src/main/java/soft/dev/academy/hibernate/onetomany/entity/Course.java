63f08473842ebcc632a2a0ac2b6a01a05e4041b0
JanLewandowsi <lewandowski.jan93@gmail.com>
2018-06-13 20:08:00 +0200

    #7 Spring-mvc demo app - MySQL and Hibernate populate the DB

--- src/main/java/soft/dev/academy/hibernate/onetomany/entity/Course.java
+++ src/main/java/soft/dev/academy/hibernate/onetomany/entity/Course.java
@@ -32,9 +32,9 @@ public class Course {
     public Course() {
     }
 
-    public Course(String title, Instructor instructor) {
+    public Course(String title) {
         this.title = title;
-        this.instructor = instructor;
+
     }
 
     public int getId() {
