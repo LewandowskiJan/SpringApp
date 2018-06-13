cc7c0abc1bde9d27345b604d503fd28c3f5f6237
JanLewandowsi <lewandowski.jan93@gmail.com>
2018-06-13 20:00:39 +0200

    #7 Spring-mvc demo app - MySQL and Hibernate configuration DB and @OneToMany relation between the Entities

---
+++ src/main/java/soft/dev/academy/hibernate/onetomany/entity/Course.java
@@ -0,0 +1,71 @@
+package soft.dev.academy.hibernate.onetomany.entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "course")
+public class Course {
+
+    //define our fields
+
+    // define constructor
+
+    // define getter setter
+
+    // define toString
+
+    // annotate fields
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "title")
+    private String title;
+
+    @ManyToOne(cascade = {CascadeType.DETACH, CascadeType.MERGE, CascadeType.PERSIST, CascadeType.REFRESH})
+    @JoinColumn(name = "instructor_id")
+    private Instructor instructor;
+
+
+    public Course() {
+    }
+
+    public Course(String title, Instructor instructor) {
+        this.title = title;
+        this.instructor = instructor;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Instructor getInstructor() {
+        return instructor;
+    }
+
+    public void setInstructor(Instructor instructor) {
+        this.instructor = instructor;
+    }
+
+    @Override
+    public String toString() {
+        return "Course{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                '}';
+    }
+}
---
+++ src/main/java/soft/dev/academy/hibernate/onetomany/repository/DeleteInstructorDetailDemo.java
@@ -0,0 +1,67 @@
+package soft.dev.academy.hibernate.onetomany.repository;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import soft.dev.academy.hibernate.onetomany.entity.Instructor;
+import soft.dev.academy.hibernate.onetomany.entity.InstructorDetail;
+
+
+public class DeleteInstructorDetailDemo {
+
+    public static void main(String[] args) {
+
+        // create session factory
+        SessionFactory factory = new Configuration()
+                .configure("hibernateonetomany.cfg.xml")
+                .addAnnotatedClass(Instructor.class)
+                .addAnnotatedClass(InstructorDetail.class)
+                .buildSessionFactory();
+
+        // create session
+        Session session = factory.getCurrentSession();
+
+        try {
+
+            // start a transaction
+            session.beginTransaction();
+
+            // get the instructor detail object
+            int theId = 5;
+            InstructorDetail tempInstructorDetail =
+                    session.get(InstructorDetail.class, theId);
+
+            // print the instructor detail
+            System.out.println("tempInstructorDetail: " +
+                    tempInstructorDetail);
+
+            // print the associated instructor
+            System.out.println("the associated instructor: " +
+                    tempInstructorDetail.getInstructor());
+
+            // now let's delete the instructor detail
+            System.out.println("Deleting tempInstructorDetail: " +
+                    tempInstructorDetail);
+
+            // remove the associated object reference
+            // break bi-directional link
+            tempInstructorDetail.getInstructor().setInstructorDetail(null);
+
+            session.delete(tempInstructorDetail);
+
+            // commit transaction
+            session.getTransaction().commit();
+
+            System.out.println("Done!");
+        } catch (Exception exc) {
+            exc.printStackTrace();
+
+        } finally {
+            
+//            session.close();
+            factory.close();
+        }
+
+    }
+
+}
